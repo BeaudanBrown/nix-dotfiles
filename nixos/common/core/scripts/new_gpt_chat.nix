13f529bc99e4607886a946fd9c01927cf5803360
Beaudan Brown <beaudan.brown@gmail.com>
2024-10-30 00:29:11 +1100

    Add forgotten lock update, delete vim-ai and make vim-ai chats save automatically

--- nixos/common/core/scripts/new_gpt_chat.nix
+++ nixos/common/core/scripts/new_gpt_chat.nix
@@ -7,8 +7,11 @@ cp "$HOME"/.local/share/gpt/default.aichat "$HOME"/.local/share/gpt/"$timestamp"
 cp "$HOME"/.local/share/gpt/o1-mini.aichat "$HOME"/.local/share/gpt/"$timestamp"-o1.aichat
 chmod 755 "$HOME"/.local/share/gpt/"$timestamp".aichat
 chmod 755 "$HOME"/.local/share/gpt/"$timestamp"-o1.aichat
-nvim -c 'normal G' -c 'startinsert!' -O "$HOME"/.local/share/gpt/"$timestamp".aichat \
-  "$HOME"/.local/share/gpt/"$timestamp"-o1.aichat
+nvim \
+  -c "autocmd BufLeave <buffer> silent! write" \
+  -c 'normal G' \
+  -c 'startinsert!' \
+  -O "$HOME"/.local/share/gpt/"$timestamp".aichat "$HOME"/.local/share/gpt/"$timestamp"-o1.aichat
 
   '';
 
