5010a7f92a8872394701745b6c5e1b329d3029ce
Beaudan Brown <beaudan.brown@gmail.com>
2025-03-30 23:35:37 +1100

    Add to wheel group and clean flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       # see: https://github.com/nix-community/home-manager/pull/3454
       lib = nixpkgs.lib.extend (self: super: { custom = import ./lib { inherit (nixpkgs) lib; }; });
     in
-    {
+      {
       nixosConfigurations = builtins.listToAttrs (
         map (host: {
           name = host;
@@ -20,63 +20,7 @@
         }) (builtins.attrNames (builtins.readDir ./hosts))
       );
     };
-    # let
-    #   inherit (nixpkgs) lib;
-    #   configLib = import ./lib { inherit lib; };
-    #   specialArgs = {
-    #     inherit
-    #     inputs
-    #     configLib
-    #     nixpkgs
-    #     ;
-    #   };
-    # in
-    # {
 
-
-    # nixpkgs.overlays = [
-    # ];
-
-    # nixosConfigurations = {
-    #   grill = nixpkgs.lib.nixosSystem {
-    #     system = "x86_64-linux";
-    #     inherit specialArgs;
-    #     modules = [
-    #       ./nixos/grill
-
-    #       nixvim.nixosModules.nixvim
-    #       stylix.nixosModules.stylix
-    #       home-manager.nixosModules.home-manager
-    #       {
-    #         home-manager.useGlobalPkgs = true;
-    #         home-manager.useUserPackages = true;
-    #         home-manager.backupFileExtension = "backup";
-    #         home-manager.users.beau = import ./home/grill;
-    #         home-manager.extraSpecialArgs = specialArgs;
-    #       }
-    #     ];
-    #   };
-
-    #   nix-laptop = nixpkgs.lib.nixosSystem {
-    #     system = "x86_64-linux";
-    #     inherit specialArgs;
-    #     modules = [
-    #       ./nixos/laptop
-
-    #       nixvim.nixosModules.nixvim
-    #       stylix.nixosModules.stylix
-    #       home-manager.nixosModules.home-manager
-    #       {
-    #         home-manager.useGlobalPkgs = true;
-    #         home-manager.useUserPackages = true;
-    #         home-manager.backupFileExtension = "backup";
-    #         home-manager.users.beau = import ./home/laptop;
-    #         home-manager.extraSpecialArgs = specialArgs;
-    #       }
-    #     ];
-    #   };
-    # };
-  # };
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org/"
@@ -85,9 +29,9 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs.config.allowUnfree = true;
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -100,11 +44,6 @@
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- modules/nixos/common/user/default.nix
+++ modules/nixos/common/user/default.nix
@@ -7,6 +7,7 @@
   users.users.${config.hostSpec.username} = {
     isNormalUser = true;
     home = "/home/${config.hostSpec.username}";
+    extraGroups = [ "wheel" ];
     group = "users";
     shell = pkgs.zsh;
     uid = 1000;
