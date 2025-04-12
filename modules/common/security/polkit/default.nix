ee450fadbbca3be7567b48c3a99710af1d622c13
Beaudan Brown <beaudan.brown@gmail.com>
2025-04-13 00:37:47 +1000

    Fix naming issue

--- modules/common/security/polkit/default.nix
+++ modules/common/security/polkit/default.nix
@@ -1,38 +1,6 @@
-{ config, ... }:
+{ ... }:
 {
-  security.sudo.extraRules = [
-    {
-      groups = [ "wheel" ];
-      commands = [
-        {
-          command = "/run/current-system/sw/bin/nh";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "/etc/profiles/per-user/${config.hostSpec.username}/bin/shutdown";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "/run/current-system/sw/bin/nixos-rebuild";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "/run/current-system/sw/bin/reboot";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "/home/${config.hostSpec.username}/.nix-profile/bin/reboot";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "/run/current-system/sw/bin/shutdown";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "/home/${config.hostSpec.username}/.nix-profile/bin/shutdown";
-          options = [ "NOPASSWD" ];
-        }
-      ];
-    }
-  ];
+  security = {
+    polkit.enable = true;
+  };
 }
