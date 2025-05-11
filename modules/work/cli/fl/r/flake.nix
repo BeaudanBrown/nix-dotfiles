79cd5f03507dad73f176b12adaaa8c19e89cd824
Beaudan Brown <beaudan.brown@gmail.com>
2025-05-11 15:29:54 +1000

    Add pre commit format hook for r flake

--- modules/work/cli/fl/r/flake.nix
+++ modules/work/cli/fl/r/flake.nix
@@ -1,20 +1,43 @@
 {
   outputs =
-    { nixpkgs, flake-utils, ... }:
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pre-commit-hooks,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              air-fmt = {
+                enable = true;
+                entry = "air format";
+                files = ".*\.[rR]$";
+              };
+            };
+          };
+        };
         devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
           env.R_LIBS_USER = "./.Rlib";
-          buildInpus = [ pkgs.bashInteractive ];
+          buildInpus = [
+            pkgs.bashInteractive
+            self.checks.${system}.pre-commit-check.enabledPackages
+          ];
           packages =
             with pkgs;
             [
               R
               quarto
+              air-formatter
             ]
             ++ (with rPackages; [
               languageserver
@@ -27,6 +50,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     systems.url = "github:nix-systems/default";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
