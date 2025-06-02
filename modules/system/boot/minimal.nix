19ad004c32fef128321204cbb0f5466de081e609
Beaudan Brown <beaudan.brown@gmail.com>
2025-06-02 22:15:29 +1000

    Try grub again

--- modules/system/boot/minimal.nix
+++ modules/system/boot/minimal.nix
@@ -10,10 +10,29 @@
     loader = {
       timeout = 1;
       efi.canTouchEfiVariables = true;
-      systemd-boot = {
+      systemd-boot.enable = false;
+      grub = {
         enable = true;
+        efiSupport = true;
+        device = "nodev";
         configurationLimit = 5;
       };
     };
   };
+  # boot = {
+  #   supportedFilesystems = [ "ntfs" ];
+  #   kernelPackages = pkgs.linuxPackages_latest;
+  #   kernelParams = [
+  #     "snd-intel-dspcfg.dsp_driver=1"
+  #     "kvm.enable_virt_at_load=0"
+  #   ];
+  #   loader = {
+  #     timeout = 1;
+  #     efi.canTouchEfiVariables = true;
+  #     systemd-boot = {
+  #       enable = true;
+  #       configurationLimit = 5;
+  #     };
+  #   };
+  # };
 }
