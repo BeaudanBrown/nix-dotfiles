4861c5a0cf1625c5bcfb63bf554015d37cdeaa30
Beaudan Brown <beaudan.brown@gmail.com>
2025-06-02 20:55:34 +1000

    Add ssh config and rbw unstable

--- modules/services/ssh/hm_minimal.nix
+++ modules/services/ssh/hm_minimal.nix
@@ -7,6 +7,25 @@
         hostname = "192.168.1.103";
         user = "root";
       };
+      pizero = {
+        hostname = "192.168.1.103";
+        user = "pi";
+        port = 22;
+      };
+      pi4 = {
+        hostname = "192.168.1.122";
+        user = "beau";
+        port = 8023;
+      };
+      dad = {
+        hostname = "slippers.beaudan.me";
+        user = "steve";
+        port = 9022;
+      };
+      m3 = {
+        hostname = "m3.massive.org.au";
+        user = "beaudanc";
+      };
     };
   };
 }
--- modules/tools/rbw/hm_work.nix
+++ modules/tools/rbw/hm_work.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, nixpkgsUnstable, ... }:
+let
+  pkgsUnstable = nixpkgsUnstable.legacyPackages.${pkgs.system};
+in
 {
   programs.rbw = {
     enable = true;
+    package = pkgsUnstable.rbw;
     settings = {
       base_url = "https://pw.beaudan.me";
       email = "beaudan.brown@gmail.com";
