3a6e4cc7e3995ea1cad77498ef570dd292d65794
Beaudan Brown <beaudan.brown@gmail.com>
2025-06-02 21:44:48 +1000

    Move boot to minimal and back to systemd-boot

---
+++ modules/system/boot/minimal.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  boot = {
+    supportedFilesystems = [ "ntfs" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [
+      "snd-intel-dspcfg.dsp_driver=1"
+      "kvm.enable_virt_at_load=0"
+    ];
+    loader = {
+      timeout = 1;
+      efi.canTouchEfiVariables = true;
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 5;
+      };
+    };
+  };
+}
