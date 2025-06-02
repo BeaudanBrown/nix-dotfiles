12c669ea8a6601b1094bab8bc980ad96932861b6
Beaudan Brown <beaudan.brown@gmail.com>
2025-06-02 23:50:31 +1000

    zsh and make aliases common

--- modules/system/disko/nas.nix
+++ modules/system/disko/nas.nix
@@ -1,9 +1,20 @@
 { ... }:
 {
-  disko.devices = (
-    import ./btrfs.nix {
+  disko.devices = import ./btrfs.nix {
       deviceName = "/dev/disk/by-id/wwn-0x5002538e00000000";
       swapSize = "16G";
-    }
-  );
+    } // {
+      zpool = {
+        pool1 = {
+          type = "zpool";
+          datasets = {
+            "pool1" = {
+              type = "zfs_fs";
+              mountpoint = "/pool1";
+              options.mountpoint = "legacy";
+            };
+          };
+        };
+      };
+    };
 }
