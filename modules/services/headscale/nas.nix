96aab0b2b40bf87069eccb4e184564eedcdd347a
Beaudan Brown <beaudan.brown@gmail.com>
2025-08-10 21:36:24 +1000

    Fix secret names

--- modules/services/headscale/nas.nix
+++ modules/services/headscale/nas.nix
@@ -38,17 +38,17 @@
         only_start_if_oidc_is_available = true;
         issuer = "https://auth.bepis.lol/application/o/headscale/";
         client_id = "FwfGvUzRKRPQWyFsV3PzKA0eh0T5qctUp8o0hpBL";
-        client_secret_path = config.sops.secrets.headscale_authentic_secret.path;
+        client_secret_path = config.sops.secrets."headscale/authentik_secret".path;
       };
     };
   };
   # Client secret key generated through authentik
-  sops.secrets.headscale_authentic_secret = {
+  sops.secrets."headscale/authentik_secret" = {
     mode = "0600";
     owner = "headscale";
     group = "headscale";
   };
-  sops.secrets.headscale_noise = {
+  sops.secrets."headscale/noise" = {
     path = "/var/lib/headscale/noise_private.key";
     mode = "0600";
     owner = "headscale";
