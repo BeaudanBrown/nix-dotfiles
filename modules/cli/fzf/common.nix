c096a8309fe08c708420443dc62b6bec4f622ae1
Beaudan <beaudan.brown@gmail.com>
2025-11-21 16:34:47 +1100

    Add fzf gitignore support and zsh vi mode fixes

--- modules/cli/fzf/common.nix
+++ modules/cli/fzf/common.nix
@@ -1,6 +1,36 @@
-{ ... }:
+{ pkgs, ... }:
 {
   hm.programs.fzf = {
     enable = true;
+    enableZshIntegration = true;
+
+    # Use fd for file and directory search (respects .gitignore by default)
+    defaultCommand = "${pkgs.fd}/bin/fd --type f --hidden --follow --exclude .git";
+    fileWidgetCommand = "${pkgs.fd}/bin/fd --type f --hidden --follow --exclude .git";
+    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d --hidden --follow --exclude .git";
+
+    # Default options for all fzf invocations
+    defaultOptions = [
+      "--height=40%"
+      "--layout=reverse"
+      "--border=none"
+    ];
+
+    # Options for Ctrl+T (file widget)
+    fileWidgetOptions = [
+      "--prompt=GIT> "
+      "--header=CTRL-G: toggle gitignore"
+      "--bind=ctrl-g:reload(fd --type f --hidden --follow --exclude .git --no-ignore)+change-prompt(ALL> )+change-header(CTRL-G: toggle gitignore)"
+    ];
+
+    # Options for Alt+C (change directory widget)
+    changeDirWidgetOptions = [
+      "--prompt=GIT> "
+      "--header=CTRL-G: toggle gitignore"
+      "--bind=ctrl-g:reload(fd --type d --hidden --follow --exclude .git --no-ignore)+change-prompt(ALL> )+change-header(CTRL-G: toggle gitignore)"
+    ];
   };
+
+  # Ensure fd is available
+  hm.home.packages = [ pkgs.fd ];
 }
--- modules/cli/zsh/minimal.nix
+++ modules/cli/zsh/minimal.nix
@@ -44,6 +44,37 @@
           bindkey -M menuselect 'l' vi-forward-char
           bindkey -M menuselect 'j' vi-down-line-or-history
 
+          # Fix backspace behavior in vi insert mode
+          # Allow backspace to delete past the point where insert mode was entered
+          bindkey -M viins '^?' backward-delete-char
+          bindkey -M viins '^H' backward-delete-char
+
+          # Ctrl+P: fzf open file or cd into directory (respects .gitignore by default)
+          # Press Ctrl+G while in fzf to toggle between respecting/ignoring .gitignore
+          # If file selected: open in $EDITOR, if directory selected: cd into it
+          fzf-open-file-or-dir() {
+            local out
+            out=$(fd --hidden --follow --exclude .git 2> /dev/null | \
+              fzf --exit-0 \
+                --height=40% \
+                --layout=reverse \
+                --border=none \
+                --prompt="GIT> " \
+                --header="CTRL-G: toggle gitignore" \
+                --bind "ctrl-g:reload(fd --hidden --follow --exclude .git --no-ignore)+change-prompt(ALL> )+change-header(CTRL-G: toggle gitignore)")
+
+            if [ -f "$out" ]; then
+              ''${EDITOR:-vim} "$out" < /dev/tty
+              zle reset-prompt
+            elif [ -d "$out" ]; then
+              zle push-line
+              BUFFER="builtin cd -- ''${(q)out}"
+              zle accept-line
+            fi
+          }
+          zle -N fzf-open-file-or-dir
+          bindkey '^P' fzf-open-file-or-dir
+
           if [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]]; then
             tmux new -A -s default &> /dev/null
           fi
