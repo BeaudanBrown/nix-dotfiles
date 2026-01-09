0e5b858cc1b909ddba532febe7fa28294a223527
Beaudan Brown <beaudan.brown@gmail.com>
2026-01-09 13:11:12 +1100

    Add gh tool

--- modules/cli/git/minimal.nix
+++ modules/cli/git/minimal.nix
@@ -16,4 +16,23 @@
       };
     };
   };
+
+  sops.secrets."gh-hosts" = {
+    owner = config.hostSpec.username;
+    inherit (config.users.users.${config.hostSpec.username}) group;
+    mode = "0600";
+    path = "${config.hostSpec.home}/.config/gh/hosts.yml";
+  };
+
+  systemd.tmpfiles.rules = [
+    "d ${config.hostSpec.home}/.config/gh 0700 ${config.hostSpec.username} users - -"
+  ];
+
+  hm.programs.gh = {
+    enable = true;
+    settings = {
+      git_protocol = "ssh";
+      editor = "vim";
+    };
+  };
 }
