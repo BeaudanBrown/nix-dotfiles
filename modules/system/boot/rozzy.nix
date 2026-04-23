1a625b966d2c4af7ed3689083b404681aab36cd4
Beaudan Brown <beaudan.brown@gmail.com>
2026-04-23 11:03:34 +1000

    Add tailscale to client

--- modules/system/boot/rozzy.nix
+++ modules/system/boot/rozzy.nix
@@ -20,5 +20,10 @@
       };
       timeout = lib.mkOverride 60 10;
     };
+    kernel.sysctl = {
+      "net.ipv6.conf.all.disable_ipv6" = 1;
+      "net.ipv6.conf.default.disable_ipv6" = 1;
+      "net.ipv6.conf.eth0.disable_ipv6" = 1;
+    };
   };
 }
