e9ab01ad0515607cd3ada2d3bf990c84ad63b5c5
beaudan <beaudan.brown@gmail.com>
2026-06-06 12:25:42 +1000

    Adjust work autofs Monash mount path

--- modules/tools/autofs/work.nix
+++ modules/tools/autofs/work.nix
@@ -16,12 +16,18 @@
     };
   };
 
+  systemd.tmpfiles.rules = [
+    "d /mnt 0755 root root - -"
+    "d /mnt/monash-shared 0755 root root - -"
+    "L+ /s - - - - /mnt/monash-shared"
+  ];
+
   services.autofs = {
     enable = true;
     autoMaster =
       let
         cifsConf = pkgs.writeText "auto.cifs" ''
-          /s -fstype=cifs,credentials=${config.sops.secrets.smbcredentials.path},uid=1000,gid=1000,iocharset=utf8,sec=ntlmssp,_netdev,nounix,cache=strict,vers=3.0,actimeo=60 ://ad.monash.edu/shared/Epi-Dementia
+          /mnt/monash-shared -fstype=cifs,credentials=${config.sops.secrets.smbcredentials.path},uid=1000,gid=1000,iocharset=utf8,sec=ntlmssp,_netdev,nounix,cache=strict,vers=3.0,actimeo=60 ://ad.monash.edu/shared/Epi-Dementia
         '';
         agentConf = pkgs.writeText "auto.agent-nfs" ''
           ${config.hostSpec.home}/agent -fstype=nfs4,rw,hard,noatime,proto=tcp,port=2049,_netdev ${config.hostSpecs.nas.tailIP}:/pool1/agent
