0666e710ba85b9ade17b3fa38a8b535ff8df7389
Beaudan Brown <beaudan.brown@gmail.com>
2026-06-11 16:59:45 +1000

    Update bepis and make grill use tmpfs

--- modules/system/boot/grill.nix
+++ modules/system/boot/grill.nix
@@ -1,3 +1,6 @@
 { ... }:
 {
+  # Grill has enough RAM to keep /tmp in memory. This is host-local so other
+  # machines keep their existing persistent /tmp behaviour.
+  boot.tmp.useTmpfs = true;
 }
