79796f1067f4ab4a8bca4e3af235392ba2272d66
Beaudan Brown <beaudan.brown@gmail.com>
2026-07-24 09:32:30 +1000

    Remove ghostty notifications

--- modules/apps/ghostty/client.nix
+++ modules/apps/ghostty/client.nix
@@ -10,7 +10,10 @@
     settings = {
       confirm-close-surface = false;
       bell-features = "no-audio";
+      app-notifications = "no-clipboard-copy";
+      clipboard-read = "allow";
       clipboard-write = "allow";
+      clipboard-paste-protection = false;
       copy-on-select = "clipboard";
       custom-shader = "${./cursor-tail-rounded.glsl}";
     };
